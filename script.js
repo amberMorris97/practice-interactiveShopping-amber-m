6a4f987edb4fb7ffa187fc05fedb653f8790319d
ambermorris97 <ambermorris1997@gmail.com>
2026-06-15 17:40:22 -0400

    create add and edit functionalities

---
+++ script.js
@@ -0,0 +1,57 @@
+let newItemInput = document.getElementById('newItem');
+let shoppingList = document.getElementById('shoppingList');
+let addItem = document.getElementById('add');
+
+let newItem = '';
+let editedItem = '';
+
+newItemInput.addEventListener('input', (event) => newItem = event.target.value);
+
+addItem.addEventListener('click', () => {
+    let editBtn = document.createElement('button');
+    editBtn.textContent = 'edit';
+    editBtn.classList.add('edit');
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'delete';
+    deleteBtn.classList.add('delete');
+
+    let newListItem = document.createElement('li');
+    newListItem.append(newItem, ' ', editBtn, ' ', deleteBtn);
+
+    shoppingList.appendChild(newListItem);
+    newItemInput.value = '';
+});
+
+shoppingList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('edit')) {
+        let saveBtn = document.createElement('button');
+        saveBtn.innerText = 'save';
+        saveBtn.classList.add('save');
+
+        let tempInput = document.createElement('input');
+        tempInput.value = event.target.parentElement.childNodes[0].textContent;
+
+        let newLi = document.createElement('li');
+        newLi.append(tempInput, ' ', saveBtn);
+        event.target.parentElement.replaceWith(newLi);
+    }
+
+    if (event.target.classList.contains('save')) {
+        let editBtn = document.createElement('button');
+        editBtn.classList.add('edit');
+        editBtn.textContent = 'edit';
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'delete';
+        deleteBtn.classList.add('delete');
+
+
+        editedItem = event.target.parentElement.childNodes[0].value;
+        let newLi = document.createElement('li');
+        newLi.append(editedItem, ' ', editBtn, ' ', deleteBtn);
+      
+        event.target.parentElement.replaceWith(newLi);
+    }
+
+});
